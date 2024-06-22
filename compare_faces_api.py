493bd80ff25f95f4d04139247b2a206f7e822fa6
bahaawanas <152633198+bahaawanas@users.noreply.github.com>
2024-06-22 19:34:31 +0300

    compare_faces_api.py

--- compare_faces_api.py
+++ compare_faces_api.py
@@ -1,36 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+import uvicorn
 import cv2
 from mtcnn import MTCNN
 from keras_facenet import FaceNet
 from scipy.spatial.distance import cosine
 import numpy as np
 import os
-from werkzeug.utils import secure_filename
+from tempfile import NamedTemporaryFile
+from shutil import copyfileobj
 
+app = FastAPI()
 
-# In[2]:
-
-
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
-
-
-# In[3]:
+UPLOAD_FOLDER = 'uploads'
 
 
 def preprocess_image(image_path):
     image = cv2.imread(image_path)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image_rgb
-
-
-# In[4]:
 
 
 def detect_and_align_face(image_rgb):
@@ -48,16 +36,10 @@ def detect_and_align_face(image_rgb):
         return None
 
 
-# In[5]:
-
-
 def get_face_embedding(face):
     embedder = FaceNet()
     embedding = embedder.embeddings([face])[0]
     return embedding
-
-
-# In[6]:
 
 
 def is_same_person(embedding1, embedding2, threshold=0.5):
@@ -65,22 +47,21 @@ def is_same_person(embedding1, embedding2, threshold=0.5):
     return distance < threshold
 
 
-# In[ ]:
+@app.post("/compare_faces")
+async def compare_faces(image1: UploadFile = File(...), image2: UploadFile = File(...)):
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    
+    image1_path = os.path.join(UPLOAD_FOLDER, image1.filename)
+    image2_path = os.path.join(UPLOAD_FOLDER, image2.filename)
+    
+    with NamedTemporaryFile(delete=False, dir=UPLOAD_FOLDER) as tmp:
+        copyfileobj(image1.file, tmp)
+        image1_path = tmp.name
 
-
-@app.route('/compare_faces', methods=['POST'])
-def compare_faces():
-    if 'image1' not in request.files or 'image2' not in request.files:
-        return jsonify({'error': 'Please provide both images'}), 400
-
-    image1 = request.files['image1']
-    image2 = request.files['image2']
-
-    image1_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(image1.filename))
-    image2_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(image2.filename))
-
-    image1.save(image1_path)
-    image2.save(image2_path)
+    with NamedTemporaryFile(delete=False, dir=UPLOAD_FOLDER) as tmp:
+        copyfileobj(image2.file, tmp)
+        image2_path = tmp.name
 
     image1_rgb = preprocess_image(image1_path)
     image2_rgb = preprocess_image(image2_path)
@@ -89,7 +70,7 @@ def compare_faces():
     face2 = detect_and_align_face(image2_rgb)
 
     if face1 is None or face2 is None:
-        return jsonify({'result': 'Face not detected in one or both images'}), 400
+        raise HTTPException(status_code=400, detail="Face not detected in one or both images")
 
     embedding1 = get_face_embedding(face1)
     embedding2 = get_face_embedding(face2)
@@ -97,396 +78,9 @@ def compare_faces():
     result = is_same_person(embedding1, embedding2)
 
     if result:
-        return jsonify({'result': 'They are the same person.'})
+        return JSONResponse(content={'result': 'They are the same person.'})
     else:
-        return jsonify({'result': 'They are not the same person.'})
+        return JSONResponse(content={'result': 'They are not the same person.'})
 
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(host='0.0.0.0', port=5000)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+    uvicorn.run(app, host='0.0.0.0', port=8000)
